4a3c383d3ad2c5bced9034d0d74eb2afce79fc23
Shishir Gautam <gautamshishir776@gmail.com>
2024-10-20 22:20:21 -0400

    Updated portfolio website with new features and design improvements

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 5005;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    await connectDB(process.env.MONGOURI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
