fb4181675c2fd31bbe1242919bfbbeb57d3ea649
Shishir Gautam <gautamshishir776@gmail.com>
2024-10-22 14:25:32 -0400

    hello

--- db/connect.js
+++ db/connect.js
@@ -2,15 +2,14 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 // Ensure that the 'url' is passed when calling connectDB
-const connectDB = async (url) => {
-  console.log("i suck at coding" );
+const connectDB =async (url) => {
   if (!url) {
     throw new Error('MongoDB connection URL is missing');
   }
 
   // Mongoose connection without additional options (default is fine in Mongoose 6+)
-   console.log(await mongoose.connect(url));
-  return mongoose.connect(url);
+   
+  return  await mongoose.connect(url);
 };
 
 module.exports = connectDB;
