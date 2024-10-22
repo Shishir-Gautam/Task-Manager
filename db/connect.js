db42c2944a408e0cda3ce0ae9a012bdd6366e1f8
Shishir Gautam <gautamshishir776@gmail.com>
2024-10-22 14:35:27 -0400

    anish sucks

--- db/connect.js
+++ db/connect.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 // Ensure that the 'url' is passed when calling connectDB
-const connectDB =async (url) => {
+const connectDB = (url) => {
   if (!url) {
     throw new Error('MongoDB connection URL is missing');
   }
 
   // Mongoose connection without additional options (default is fine in Mongoose 6+)
    
-  return  await mongoose.connect(url);
+  return   mongoose.connect(url);
 };
 
 module.exports = connectDB;
